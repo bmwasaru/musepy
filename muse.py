6b2b66c2d1585ea01dfab00090afd896d30260c3
bmwasaru <bmwasaru@gmail.com>
2014-12-20 18:45:53 +0300

    parameterize URLs

--- muse.py
+++ muse.py
@@ -1,3 +1,6 @@
+import re
+
+
 class application:
     def __init__(self, environ, start_response):
         self.environ = environ
@@ -5,31 +8,34 @@ class application:
 
     urls = [
         ("/", "index"),
-        ("/hello", "hello")
+        ("/hello/(.*)", "hello")
     ]
 
     def __iter__(self):
-        path_info = self.environ['PATH_INFO']
+        path = self.environ['PATH_INFO']
         method = self.environ['REQUEST_METHOD']
 
-        for path, name  in self.urls:
-            if path == path_info:
+        for pattern, name in self.urls:
+            m = re.match('^' + pattern + '$', path)
+            if m:
+                # matched groups as arguments
+                args = m.groups()
                 funcname = method.upper() + "_" + name
                 func = getattr(self, funcname)
-                return func()
+                return func(*args)
         return self.not_found()
-    
+
     def index(self):
         status = "200 OK"
         response_headers = [('Content-type', 'text/html')]
         self.start(status, response_headers)
         yield "Welcome!\n"
 
-    def hello(self):
+    def hello(self, name):
         status = "200 OK"
         response_headers = [('Content-type', 'text/html')]
         self.start(status, response_headers)
-        yield "Hello world\n"
+        yield "Hello %s\n" % name
 
     def not_found(self):
         status = "404 Not Found"
