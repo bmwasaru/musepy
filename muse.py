b52b64d1ee265490612c8d49af7fb33eac4e3f47
bmwasaru <bmwasaru@gmail.com>
2014-12-20 11:56:04 +0300

    class better than function to handle 2 URLs

--- muse.py
+++ muse.py
@@ -1,6 +1,10 @@
-def application(environ, start_response):
-    """Simplest possible application object"""
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    return ['Hello world!\n']
+class application:
+    def __init__(self, environ, start_response):
+        self.environ = environ
+        self.start = start_response
+
+    def __iter__(self):
+        status = "200 OK"
+        response_headers = [('Content-type', 'text/html')]
+        self.start(status, response_headers)
+        yield "Hello world\n"
