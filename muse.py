2cbfd0a7cd8ef2b5311354fcca15f0153341b407
bmwasaru <bmwasaru@gmail.com>
2014-12-20 11:46:08 +0300

    simplest possible app object

---
+++ muse.py
@@ -0,0 +1,6 @@
+def application(environ, start_response):
+    """Simplest possible application object"""
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    return ['Hello world!\n']
