a732abcba7c3393657d2d1112c7f2d0671629305
Britone Mwasaru <bmwasaru@gmail.com>
2020-05-12 09:30:32 +0300

    support keyword parameters in the url path

--- app.py
+++ app.py
@@ -11,3 +11,8 @@ def home(request, response):
 @app.route('/about/')
 def about(request, response):
     response.text = "This is the about page"
+
+
+@app.route('/profile/{name}')
+def profile(request, response, name):
+    response.text = f"Hello {name}"
--- muse/api.py
+++ muse/api.py
@@ -1,4 +1,5 @@
 from webob import Request, Response
+from parse import parse
 
 
 class API:
@@ -26,9 +27,9 @@ class API:
     def handle_request(self, request):
         response = Response()
 
-        handler = self.find_handler(request_path=request.path)
+        handler, kwargs = self.find_handler(request_path=request.path)
         if handler is not None:
-            handler(request, response)
+            handler(request, response, **kwargs)
         else:
             self.default_response(response)
 
@@ -36,5 +37,7 @@ class API:
 
     def find_handler(self, request_path):
         for path, handler in self.routes.items():
-            if path == request_path:
-                return handler
+            parse_result = parse(path, request_path)
+            if parse_result is not None:
+                return handler, parse_result.named
+        return None, None
