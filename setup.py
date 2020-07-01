5ba9c5cf5b2769f4d91e9eeb2fd5d2a3e9615252
Britone Mwasaru <bmwasaru@gmail.com>
2020-07-01 05:16:16 +0300

    add framework author details

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-import os
 from setuptools import setup, find_packages
+
+from muse import __version__
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
     name='muse',
-    version='0.1',
+    version=__version__,
+    author="Britone Mwasaru",
+    author_email="bmwasaru@gmail.com",
+    license="Public domain",
     description='Python microframework',
     packages=find_packages(exclude=['demo']),
     install_requires=required,
-    keywords='python microframework'
+    keywords='python microframework',
+    platforms=["any"]
 )
